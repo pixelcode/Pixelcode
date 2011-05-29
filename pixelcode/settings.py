feb5e9a051a56287f74dd4b8bfa85beb1fea80b7
Ramsey Dsilva <ramsey@pixelcode.ca>
2011-05-29 07:53:17 -0400

    renamed db

--- pixelcode/settings.py
+++ pixelcode/settings.py
@@ -1,22 +1,13 @@
 # Django settings for Votrasi project.
 import os
 
+from custom_settings import *
+
 ADMINS = (
     ('Ramsey', 'ramsey@pixelcode.ca'),
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'pixelcode.sqlite', # Or path to database file if using sqlite3.
-        'USER': 'root', # Not used with sqlite3.
-        'PASSWORD': '', # Not used with sqlite3.
-        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '', # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 TIME_ZONE = 'America/Chicago'
 
