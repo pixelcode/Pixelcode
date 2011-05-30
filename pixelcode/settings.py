fea1f0fc9ee0e5dcc3ab816a4c2c726baa10c1c5
Ramsey Dsilva <ramsey@pixelcode.ca>
2011-05-30 03:14:07 -0400

    Fixed tinymce

--- pixelcode/settings.py
+++ pixelcode/settings.py
@@ -70,9 +70,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'pixelcode.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), 'templates')
 )
 
 
